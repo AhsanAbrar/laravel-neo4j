e049912607dd741fa4fb78c7b69816803f21e698
Muhammad Ahsan Abrar <iahsanabrar@gmail.com>
2018-07-25 15:36:56 +0500

    test service provider

---
+++ src/Neo4jServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ahsan\Neo4j;
+
+use Illuminate\Support\ServiceProvider;
+
+class Neo4jServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+}

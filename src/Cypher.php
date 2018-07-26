288cf92e55e4aa68b597418f7897347ba544667d
Muhammad Ahsan Abrar <iahsanabrar@gmail.com>
2018-07-26 22:58:43 +0500

    first test setup

--- src/Cypher.php
+++ src/Cypher.php
@@ -2,20 +2,9 @@
 
 namespace Ahsan\Neo4j;
 
-use GraphAware\Neo4j\Client\ClientBuilder;
-use Illuminate\Config\Repository as Config;
-
 abstract class Cypher
 {
-	/**
-	 * The Neo4j Graphaware Client
-	 */
-	protected $client;
-
-	/**
-	 * Configurations
-	 */
-	protected $config;
+	use Concerns\Connection;
 
 	/**
 	 * Current Model
@@ -23,32 +12,11 @@ abstract class Cypher
 	protected $model;
 
 	/**
-	 * Constructor()
-	 */
-	function __construct(Config $config)
-	{
-		$this->config = $config;
-
-		$this->setClient();
-	}
-
-	/**
-	 * Set Client First
-	 */
-	public function setClient()
-	{
-		$this->client = ClientBuilder::create()
-			->addConnection($this->config->get('app.name'), 'http://neo4j:root@localhost:7474')
-			//->addConnection('bolt', 'bolt://neo4j:root@localhost:7687')
-			->build();
-	}
-
-	/**
 	 * neo4j query
 	 */
-	public function query($queryString)
+	public function query($queryString, $param = [])
 	{
-		return $this->client->run($queryString);
+		return $this->client->run($queryString, $param);
 	}
 
 	public function first()

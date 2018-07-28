84c2746abb87cbfd1bcb251729dd563646e88f52
Muhammad Ahsan Abrar <iahsanabrar@gmail.com>
2018-07-28 13:51:18 +0500

    raw query

--- src/Cypher.php
+++ src/Cypher.php
@@ -21,11 +21,69 @@ class Cypher
 	protected $table;
 
 	/**
+	 * The Neo4j query string
+	 */
+	protected $queryString;
+
+	/**
+	 * The Neo4j query parameters
+	 */
+	protected $queryParam;
+
+	/**
+	 * The Graphaware ResultSet
+	 */
+	protected $result;
+
+	/**
 	 * neo4j query
 	 */
 	public function query($queryString, $param = [])
 	{
 		return $this->client->run($queryString, $param);
+	}
+
+	/**
+	 * neo4j query
+	 */
+	public function rawQuery($model, $queryString, $param = [])
+	{
+		$model = 'App\\' . $model;
+		$this->model = new $model();
+
+		$this->queryString = $queryString;
+		$this->queryParam = $param;
+
+		return $this;
+	}
+
+	public function rawFirst()
+	{
+		$result = $this->query($this->queryString, $this->queryParam);
+
+		$attributes = $result->getRecord()->values()[0]->values();
+		$attributes['id'] = $result->getRecord()->values()[0]->identity();
+
+		return $this->model->newFromBuilder($attributes);
+	}
+
+	public function rawGet()
+	{
+		$result = $this->query($this->queryString, $this->queryParam);
+
+		$models = [];
+
+		if (!$result->getRecords())
+			return Collection::make($models);
+
+		foreach ($result->getRecords() as $record) {
+			$attributes = $record->values()[0]->values();
+			$attributes['id'] = $record->values()[0]->identity();
+
+			$models[] = $this->model->newFromBuilder($attributes);
+		}
+
+		return Collection::make($models);
 	}
 
 	public function first($result)
